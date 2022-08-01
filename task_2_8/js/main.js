4e7ef98920ea4144b68a9f30a872720d4a57a7a4
V Zhevaha <v.zhevaga@gmail.com>
2022-08-01 22:24:54 +0200

    Added bag section

--- task_2_8/js/main.js
+++ task_2_8/js/main.js
@@ -2,12 +2,16 @@
 
 // Burger =============================================
 const burger = document.querySelector('.burger');
+const bagBurger = document.querySelector('.bag__burger');
+const backet = document.querySelector('.menu__backet');
+const bag = document.querySelector('.bag');
 const menuLink = document.querySelector('.menu__list');
 const menuLinks = document.querySelectorAll('.menu__link');
 const fixedBlocks = document.querySelectorAll('.fixed');
 
 if (burger) {
 	burger.addEventListener('click', (e) => {
+		console.log('element: ' + burger.className);
 		let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
 		if (document.body.classList.contains('lock')) {
 			burger_close();
@@ -17,13 +21,31 @@ if (burger) {
 	});
 }
 
+if (backet) {
+	backet.addEventListener('click', (e) => {
+		e.preventDefault();
+		console.log('element: ' + backet.className);
+		bagBurger.classList.add('active');
+		bag.classList.add('active');
+	});
+}
+
+if (bagBurger) {
+	bagBurger.addEventListener('click', (e) => {
+		e.preventDefault();
+		console.log('element: ' + bagBurger.className);
+		bagBurger.classList.remove('active');
+		bag.classList.remove('active');
+	});
+}
+
 function burger_open(element) {
 	if (fixedBlocks) {
 		fixedBlocks.forEach((el) => {
 			el.style.paddingRight = element;
 		});
 	}
-	// document.body.style.paddingRight = element;
+	document.body.style.paddingRight = element;
 	document.body.classList.add('lock');
 	burger.classList.add('active');
 	menuLink.classList.add('active');
